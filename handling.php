fa74b225e6e3b04b7b07bfed89351ba32ff36e2e
funny97 <57888178+funny97@users.noreply.github.com>
2019-11-19 13:28:46 +0800

    funny-add handling + createDB file

--- handling.php
+++ handling.php
@@ -1,42 +1,30 @@
 <?php
 
-$servername="127.0.0.1";
-$username="root";
-$password=null;
-
-$handler=mysqli_connect($servername,$username,$password);
-
-if(!$handler){
-  die("Connection failed! : " .mysqli_connect_error() );
-}else{
-  echo "Connected successfully";
-}
-
-if(mysqli_query($handler,'CREATE DATABASE treatDB')){
-  echo "Database created successfully";
-}else{
-  echo "Error creating database : " . mysqli_error($handler);
-}
-
-$query="CREATE TABLE userInfo(
-  if INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  fullname VARCHAR(60) NOT NULL,
-  dob DATE,
-  gender VARCHAR(6),
-  email VARCHAR(40),
-  phone VARCHAR(12),
-  add1 VARCHAR(30),
-  add2 VARCHAR(30),
-  postcode VARCHAR(5),
-  state VARCHAR(15),
-  country VARCHAR(25))"
-
-  mysqli_query($handler,$query);
-
   $servername1="127.0.0.1";
   $username1="root";
   $password1=null;
   $dbname="treatDB";
 
   $handler=mysqli_connect($servername1,$username1,$password1,$dbname);
+
+  if(!$handler){
+    die("Connection failed" . mysqli_connect_error());
+  }else{
+    echo "Connected successfully";
+  }
+
+  $username=$_POST['user_name'];
+  $email=$_POST['user_email'];
+  $password=$_POST['password'];
+  $add1=$_POST['address1'];
+  $add2=$_POST['address2'];
+  $postcode=$_POST['postcode'];
+  $state=$_POST['state'];
+
+
+  $query="INSERT INTO userInfo (fullname,email,pw,add1,add2,postcode,state)
+  VALUES ('$username','$email','$password','$add1','$add2','$postcode','$state')";
+
+  mysqli_query($handler,$query);
+
 ?>
