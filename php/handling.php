c47d3b52dae4bb5476a94bc69c7cda469e0eed02
funny97 <57888178+funny97@users.noreply.github.com>
2019-11-22 14:47:40 +0800

    funny-amended registration and login

--- php/handling.php
+++ php/handling.php
@@ -11,20 +11,26 @@
     die("Connection failed" . mysqli_connect_error());
   }else{
     echo "Connected successfully";
+
+    $username=$_POST['user_name'];
+    $email=$_POST['emailReg'];
+    $password=$_POST['passwordReg'];
+    $add1=$_POST['address1'];
+    $add2=$_POST['address2'];
+    $postcode=$_POST['postcode'];
+    $state=$_POST['state'];
+
+    $query="INSERT INTO userInfo (username,email,pw,add1,add2,postcode,state) VALUES ('$username','$email','$password','$add1','$add2','$postcode','$state')";
+    echo $query;
+    if(mysqli_query($handler,$query)){
+      echo "Data Inserted";
+      header("location:../index.php");
+    }else{
+      echo mysqli_query($handler,$query);
+      echo "WHY can INSERT?!?!?";
+    }
   }
 
-  $username=$_POST['user_name'];
-  $email=$_POST['user_email'];
-  $password=$_POST['password'];
-  $add1=$_POST['address1'];
-  $add2=$_POST['address2'];
-  $postcode=$_POST['postcode'];
-  $state=$_POST['state'];
 
-
-  $query="INSERT INTO userInfo (fullname,email,pw,add1,add2,postcode,state)
-  VALUES ('$username','$email','$password','$add1','$add2','$postcode','$state')";
-
-  mysqli_query($handler,$query);
 
 ?>
--- php/logout.php
+++ php/logout.php
@@ -2,6 +2,6 @@
 session_start();
 session_destroy();
 echo "Destroyed";
-//header("location:../index.php");
+header("location:../index.php");
 
 ?>
